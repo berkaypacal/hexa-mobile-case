9cc005097bf9f84331880a8efe3a1243d8cf0d88
berkay.pacal <berkay.pacal@mtdt.co>
2025-04-18 02:35:28 +0300

    Add colors for theme

---
+++ src/theme/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  white: "#FFFFFF",
+  dark50: "#FAFAFA",
+  dark400: "#A1A1AA",
+  dark500: "#71717A",
+  dark600: "#27272A",
+  dark1000: "#09090B",
+  dark: "#18181B",
+  purple: "#943DFF",
+  blue: "#2938DC",
+  red: "#EF4444",
+};
