1c25d8894001db696bc8abd38acb0c71ae8bc835
berkay.pacal <berkay.pacal@mtdt.co>
2025-04-18 02:42:17 +0300

    Add custom Manrope fonts

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Manrope: require("./assets/fonts/Manrope-Regular.ttf"),
+    "Manrope-Bold": require("./assets/fonts/Manrope-Bold.ttf"),
+    "Manrope-ExtraBold": require("./assets/fonts/Manrope-ExtraBold.ttf"),
+  });
+
+  if (!fontsLoaded) return null;
+
+  SplashScreen.hideAsync();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
