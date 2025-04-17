94729ef9312da6dc96577adb9d2e7c7eaa559266
berkay.pacal <berkay.pacal@mtdt.co>
2025-04-18 02:47:43 +0300

    Add images and image helpers

---
+++ src/utility/icons.js
@@ -0,0 +1,5 @@
+export const icons = {
+  star: require("../../assets/icons/star_ic.png"),
+  copy: require("../../assets/icons/copy_ic.png"),
+  dice: require("../../assets/icons/dice_ic.png"),
+};
---
+++ src/utility/images.js
@@ -0,0 +1,7 @@
+export const images = {
+  gradientBackground: require("../../assets/background/gradient_bg.png"),
+  noStyle: require("../../assets/style_icons/no_style_img.png"),
+  monogram: require("../../assets/style_icons/monogram_img.png"),
+  abstract: require("../../assets/style_icons/abstract_img.png"),
+  mascot: require("../../assets/style_icons/mascot_img.png"),
+};
