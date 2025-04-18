2da589b25a10ce6554d627b820ed8d9bd371439e
berkay.pacal <berkay.pacal@mtdt.co>
2025-04-18 03:17:30 +0300

    Update OutputScreen

--- src/screens/OutputScreen/index.js
+++ src/screens/OutputScreen/index.js
@@ -1,14 +1,21 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Image } from "react-native";
 import styles from "./styles";
+import PromptCard from "../../components/common/PromptCard";
 import GradientBackground from "../../components/common/GradientBackground";
 
-export default function OutputScreen() {
+const OutputScreen = ({ route }) => {
+  const { imageUrl, prompt } = route.params;
+
   return (
     <GradientBackground>
-      <View>
-        <Text>OutputScreen</Text>
+      <View style={styles.container}>
+        <Image source={{ uri: imageUrl }} style={styles.image} />
+        <View style={styles.spacing} />
+        <PromptCard prompt={prompt} />
       </View>
     </GradientBackground>
   );
-}
+};
+
+export default OutputScreen;
--- src/screens/OutputScreen/styles.js
+++ src/screens/OutputScreen/styles.js
@@ -1,3 +1,17 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  image: {
+    width: 342,
+    height: 342,
+    borderRadius: 20,
+    alignSelf: "center",
+  },
+  spacing: {
+    height: 24,
+  },
+});
